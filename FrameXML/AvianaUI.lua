3a9217f5ff3c1222377e70ce75e69ffc16dad38a
Hlkz <hlkz@hotmail.fr>
2015-11-29 10:04:15 +0100

    FrameXML - Start Selection

---
+++ FrameXML/AvianaUI.lua
@@ -0,0 +1,21 @@
+
+Aviana_IsLeaveWorldMapAllowed = false
+Aviana_OnClickScripts = false
+
+function ToggleWorldMapFrame()
+	if Aviana_IsLeaveWorldMapAllowed then
+		ToggleFrame(WorldMapFrame)
+	end
+end
+
+Aviana_FullScreenFlasher = CreateFrame("Frame", "AWFlash")
+local flasher = Aviana_FullScreenFlasher
+flasher:SetToplevel(true)
+flasher:SetFrameStrata("FULLSCREEN_DIALOG")
+flasher:SetAllPoints(UIParent)
+flasher:EnableMouse(false)
+flasher.texture = flasher:CreateTexture(nil, "BACKGROUND")
+local t = flasher.texture
+t:SetTexture(0, 0, 0)
+t:SetAllPoints(UIParent)
+flasher:Show()
